c94175fc2e91ebd86f8a2717f68a63a5711fa76e
Chris Joel <cdata@google.com>
2018-02-07 14:29:22 -0800

    Write a README and add a module index

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { Spec } from './spec.js';
+export { Suite } from './suite.js';
+export { ConsoleReporter } from './reporters/console-reporter.js';
+export { Fixturable } from './mixins/fixturable.js';
+export { Isolatable } from './mixins/isolatable.js';
+export { timePasses, timeLimit } from './util.js';
+
