25e40320514a4b31f2257066b03d22b651374380
Chris Joel <cdata@google.com>
2018-02-08 10:26:41 -0800

    Test timeouts in tests.

--- src/test-spec.ts
+++ src/test-spec.ts
@@ -2,6 +2,7 @@ import { Spec } from './spec.js';
 import { Fixturable } from './mixins/fixturable.js';
 import { Suite } from './suite.js';
 import { Test } from './test.js';
+import { timePasses } from './util.js';
 import '../../../chai/chai.js';
 
 const spec = new (Fixturable(Spec))();
@@ -12,7 +13,7 @@ const { describe, it, before } = spec;
 describe('Test', () => {
   before((context: any) => {
     let testRunCount = 0;
-    const testState = {
+    const testSpy = {
       get runCount() { return testRunCount; },
       implementation: () => { testRunCount++ }
     };
@@ -21,8 +22,8 @@ describe('Test', () => {
     return {
       ...context,
       suite,
-      testState,
-      test: new Test('is awesome', testState.implementation, {})
+      testSpy,
+      test: new Test('is awesome', testSpy.implementation, {})
     };
   });
 
@@ -30,9 +31,23 @@ describe('Test', () => {
     expect(test.run(suite)).to.be.instanceof(Promise);
   });
 
-  it('invokes the implementation when run', async ({ test, testState, suite }: any) => {
+  it('invokes the implementation when run', async ({ test, testSpy, suite }: any) => {
     await test.run(suite);
-    expect(testState.runCount).to.be.equal(1);
+    expect(testSpy.runCount).to.be.equal(1);
+  });
+
+  describe('when timing out', () => {
+    before((context: any) => ({
+      ...context,
+      test: new Test('is timing out',
+          async () => await timePasses(100), { timeout: 50 })
+    }));
+
+    it('eventually fails', async ({ test, suite }: any) => {
+      const result = await test.run(suite);
+      expect(result.passed).to.be.equal(false);
+      expect(result.error).to.be.instanceof(Error);
+    });
   });
 });
 
